7bdff391fc7a47625006666ded867224d73217ac
эмилия омурзакова <emiliaomurzakova1@gmail.com>
2024-04-03 22:10:21 +0600

    first

---
+++ src/components/data/Data.js
@@ -0,0 +1,225 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "Music",
+    path: "/services",
+  },
+  
+  {
+    text: "contact",
+    path: "/contact",
+  },
+]
+export const featured = [
+  {
+    cover: "../images/hero/h1.png",
+    name: "",
+    total: "",
+  },
+  {
+    cover: "../images/hero/h2.png",
+    name: " ",
+    total: "",
+  },
+  {
+    cover: "../images/hero/h3.png",
+    name: "",
+    total: "",
+  },
+  {
+    cover: "../images/hero/h4.png",
+    name: "",
+    total: "",
+  },
+  {
+    cover: "../images/hero/h6.png",
+    name: "",
+    total: "",
+  },
+]
+export const list = [
+  {
+    id: 1,
+    cover: "../images/list/p-1.png",
+    name: "Red Carpet Real Estate",
+    location: "210 Zirak Road, Canada",
+    category: "For Rent",
+    price: "$3,700",
+    type: "Apartment",
+  },
+  {
+    id: 2,
+    cover: "../images/list/p-2.png",
+    name: "Fairmount Properties",
+    location: "5698 Zirak Road, NewYork",
+    category: "For Sale",
+    price: "$9,750",
+    type: "Condos",
+  },
+  {
+    id: 3,
+    cover: "../images/list/p-7.png",
+    name: "The Real Estate Corner",
+    location: "5624 Mooker Market, USA",
+    category: "For Rent",
+    price: "$5,860",
+    type: "Offices",
+  },
+
+]
+export const awards = [
+  {
+    icon: <i class='fa-solid fa-trophy'></i>,
+    num: "32 M	",
+    name: "Blue Burmin Award",
+  },
+  {
+    icon: <i class='fa-solid fa-briefcase'></i>,
+    num: "43 M",
+    name: "Mimo X11 Award",
+  },
+  {
+    icon: <i class='fa-solid fa-lightbulb'></i>,
+    num: "51 M",
+    name: "Australian UGC Award",
+  },
+  {
+    icon: <i class='fa-solid fa-heart'></i>,
+    num: "42 M",
+    name: "IITCA Green Award",
+  },
+]
+export const location = [
+  {
+    id: 1,
+    cover: "./images/location/city-1.png",
+  },
+  {
+    id: 2,
+    cover: "./images/location/city-2.png",
+  },
+  {
+    id: 3, 
+    cover: "./images/location/city-3.png",
+  },
+  
+]
+export const team = [
+  {
+    cover: "../images/customer/team-1.jpg",
+    address: "",
+    name: "Akunova Raida",
+    icon: [ <i class='fa-brands fa-tiktok'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "70",
+    cover: "../images/customer/team-2.jpg",
+    address: "",
+    name: "Bekeeva Jibek",
+    icon: [ <i class='fa-brands fa-tiktok'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "80",
+    cover: "../images/customer/team-3.jpg",
+    address: "",
+    name: "Omurzakova Emiliya",
+    icon: [ <i class='fa-brands fa-tiktok'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+]
+export const price = [
+  {
+    plan: "Basic",
+    price: "29",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "120GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "5GB Cloud Storage",
+      },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Personal Help Support" },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Enterprise SLA" },
+    ],
+  },
+  {
+    best: "Best Value",
+    plan: "Standard",
+    price: "49",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "150GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "10GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        change: "color",
+        icon: <i class='fa-solid fa-x'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+  {
+    plan: "Platinum",
+    price: "79",
+    ptext: "2 user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "100% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "200GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "20GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+]
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [{ list: "Home Page" }, { list: "About Page" },  { list: "Single Blog" }],
+  },
+  
+  {
+    title: "COMPANY",
+    text: [{ list: "About" }, { list: "Music" },  { list: "Login" }, ],
+  },
+]
